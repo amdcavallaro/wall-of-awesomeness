d889fdcacea04f9b0746a37bef0a8c3aabcf9d12
Patrick Arminio <patrick.arminio@gmail.com>
2017-09-30 13:28:55 +0100

    Add initial intro screen

--- src/app.js
+++ src/app.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 
+import './app.css';
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <h1 className="title">GDG Wall of Awesomenewss</h1>;
   }
 }
 
