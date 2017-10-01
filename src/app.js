2982329dfa25e960594d3b419189d1d2d3a77315
Patrick Arminio <patrick.arminio@gmail.com>
2017-10-01 02:37:36 +0100

    Refactor components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Home from './pages/home';
+
 import './app.css';
 
 class App extends Component {
   render() {
-    return <h1 className="title">GDG Wall of Awesomenewss</h1>;
+    return <Home />;
   }
 }
 
---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Home extends Component {
+  render() {
+    return <h1 className="home__title">GDG Wall of Awesomeness</h1>;
+  }
+}
+
+export default Home;
