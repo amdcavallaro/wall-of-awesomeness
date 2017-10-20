0ccca83cce701e570c1ce6a3d2e7c979351e6089
Patrick Arminio <patrick.arminio@gmail.com>
2017-10-20 03:15:08 +0100

    Add Camera component

--- src/pages/camera/index.js
+++ src/pages/camera/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import Camera from '../../components/camera';
+
 import './style.css';
 
 class CameraPage extends Component {
   render() {
-    return <div className="camera">Hello Camera</div>;
+    return (
+      <div className="camera">
+        <Camera enabled={true} />
+      </div>
+    );
   }
 }
 
