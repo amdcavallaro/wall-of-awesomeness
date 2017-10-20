8cc6b1d84f5fc8c6a8cdeb8658faa3544f25800f
Patrick Arminio <patrick.arminio@gmail.com>
2017-10-20 03:12:13 +0100

    Rename Camera to CameraPage

--- src/pages/camera/index.js
+++ src/pages/camera/index.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 import './style.css';
 
-class Camera extends Component {
+class CameraPage extends Component {
   render() {
     return <div className="camera">Hello Camera</div>;
   }
 }
 
-export default Camera;
+export default CameraPage;
