09c2051fc42ccfc92a859733a6e47c465c11ecf4
Patrick Arminio <patrick.arminio@gmail.com>
2017-10-20 03:00:51 +0100

    Add empty wall page

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './pages/home';
+import Wall from './pages/wall';
 
 import './app.css';
 
@@ -12,6 +13,7 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component={Home} />
+          <Route exact path="/wall" component={Wall} />
         </div>
       </Router>
     );
