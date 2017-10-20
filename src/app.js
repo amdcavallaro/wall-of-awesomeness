d25268c8a2108f35847a8d285ab0468d7cb113dd
Patrick Arminio <patrick.arminio@gmail.com>
2017-10-20 02:57:48 +0100

    Move home page to route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Home from './pages/home';
 
 import './app.css';
 
 class App extends Component {
   render() {
-    return <Home />;
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
+    );
   }
 }
 
