10e8e0b2651e1f3a3ecdfe1cbe0720811dc4e578
Patrick Arminio <patrick.arminio@gmail.com>
2017-10-20 03:07:02 +0100

    Add empty camera page

---
+++ src/pages/camera/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Camera extends Component {
+  render() {
+    return <div className="camera">Hello Camera</div>;
+  }
+}
+
+export default Camera;
