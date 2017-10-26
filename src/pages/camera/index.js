c838ec270309214ae8e848a770b9a8aca7a450a4
Patrick Arminio <patrick.arminio@gmail.com>
2017-10-26 11:02:47 +0100

    Break camera page into multiple steps

--- src/pages/camera/index.js
+++ src/pages/camera/index.js
@@ -7,19 +7,48 @@ import Camera from '../../components/camera';
 import './style.css';
 
 class CameraPage extends Component {
+  WAITING = 0;
+  CAMERA_ENABLED = 1;
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentStep: this.WAITING,
+    };
+  }
+
+  renderWaiting() {
+    return [
+      <Title key="title">
+        Do you want to take a picture with your camera?
+      </Title>,
+      <Button
+        key="yes"
+        onClick={() => this.setState({ currentStep: this.CAMERA_ENABLED })}
+      >
+        Yes!
+      </Button>,
+      <Button key="no">No</Button>,
+    ];
+  }
+
+  renderCamera() {
+    return <Camera enabled={true} />;
+  }
+
+  renderCurrentStep() {
+    switch (this.state.currentStep) {
+      case this.WAITING:
+      default:
+        return this.renderWaiting();
+      case this.CAMERA_ENABLED:
+        return this.renderCamera();
+    }
+  }
+
   render() {
-    return (
-      <div className="camera">
-        <div>
-          <Title>Do you want to take a picture with your camera?</Title>
-
-          <Button>Yes!</Button>
-          <Button>No</Button>
-        </div>
-
-        <Camera enabled={false} />
-      </div>
-    );
+    return <div className="camera">{this.renderCurrentStep()}</div>;
   }
 }
 
