c6414891b523b56e9fac11584860d552e11ad10f
Patrick Arminio <patrick.arminio@gmail.com>
2017-11-03 23:02:06 +0000

    Improve image component

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -1,10 +1,52 @@
 import React from 'react';
 
+import { string, number } from 'prop-types';
+
 import './style.css';
 
 class Image extends React.Component {
+  static propTypes = {
+    src: string.isRequired,
+    ratio: number.isRequired,
+  };
+
+  state = {
+    loaded: false,
+  };
+
+  componentDidMount() {
+    var self = this;
+    var img = document.createElement('img');
+
+    img.onload = function() {
+      self.setState({ loaded: true });
+    };
+
+    img.src = this.props.src;
+  }
+
   render() {
-    return <img src={this.props.src} alt={this.props.alt} />;
+    let additionalClassNames = '';
+
+    if (this.state.loaded) {
+      additionalClassNames = 'image__wrapper--loaded';
+    }
+
+    return (
+      <div className={`image__wrapper ${additionalClassNames}`}>
+        <div
+          style={{
+            paddingBottom: `${Math.round(100 / Number(this.props.ratio))}%`,
+          }}
+        />
+        <div
+          className="image__image"
+          style={{
+            backgroundImage: `url(${this.props.src})`,
+          }}
+        />
+      </div>
+    );
   }
 }
 
