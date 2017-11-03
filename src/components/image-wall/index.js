e69a9ec86792f49aea6ae0bac98ccd7a32156a60
Patrick Arminio <patrick.arminio@gmail.com>
2017-11-03 22:48:06 +0000

    Add empty image component

--- src/components/image-wall/index.js
+++ src/components/image-wall/index.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
+import Image from '../image';
+
 // import './style.css';
 
 class ImageWall extends React.Component {
   render() {
     return (
       <div>
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
-        <img src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
+        <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
       </div>
     );
   }
