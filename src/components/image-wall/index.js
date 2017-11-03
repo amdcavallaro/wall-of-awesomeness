4c6c808cdf24c174b59d5870494bd85ef81bae41
Patrick Arminio <patrick.arminio@gmail.com>
2017-11-03 23:02:22 +0000

    Add some styles for the wall

--- src/components/image-wall/index.js
+++ src/components/image-wall/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Image from '../image';
 
-// import './style.css';
+import './style.css';
 
 class ImageWall extends React.Component {
   render() {
     return (
-      <div>
+      <div className="wall">
         <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
         <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
         <Image ratio={1} src="http://via.placeholder.com/350x150" alt="" />
